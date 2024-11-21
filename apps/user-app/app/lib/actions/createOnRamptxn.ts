0da53f335c414ec0f524e88d129f61e446b0dca0
Devender singh shekhawat <devendersingh2k@gmail.com>
2024-11-21 22:49:12 +0530

    onramp added

---
+++ apps/user-app/app/lib/actions/createOnRamptxn.ts
@@ -0,0 +1,30 @@
+import prisma from "@repo/db/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth";
+
+
+export async function createOnRampTransaction(amount : number, provider : string){
+
+    const session = await getServerSession(authOptions);
+    const userId = session.user.id;
+    const token = Math.random().toString();
+
+    if (!userId){
+        return {
+            msg : "User is not logged in!"
+        }
+    }
+
+    await prisma.onRampTransaction.create({
+        data : {
+            userId,
+            amount : amount,
+            status : "Processing",
+            startTime : new Date(),
+            provider : provider,
+            token : token
+
+
+        }
+    })
+}
